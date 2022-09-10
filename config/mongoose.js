38f64792a77652f75c3d12090be76c1b92027908
Tushar <tusharshingane1234@gmail.com>
2022-09-10 08:51:39 +0530

    make it better

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/contact_list_db";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/todolist_db";
 
-mongoose.connect(dbUrl);
+mongoose
+    .connect(dbUrl)
+    .then(() => {
+        console.log("Mongo Is Running");
+    })
+    .catch((err) => {
+        console.log("Mongo Error");
+    });
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', function (err) { console.log(err.message); });
+// db.on('error', function (err) { console.log(err.message); });
 
-db.once('open', function () {
-    console.log("successfully connected");
-});
+// db.once('open', function () {
+//     console.log("successfully connected");
+// });
